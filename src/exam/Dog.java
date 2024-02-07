8209d6d534b6758801f6d2f729d597579520e508
Clyde-S-Fernandes <clydejbac03@gmail.com>
2024-02-07 10:29:13 +0530

    commit

--- src/exam/Dog.java
+++ src/exam/Dog.java
@@ -1,5 +1,16 @@
 package exam;
 
 public class Dog {
+	private String name;
+	private String breed;
+	public Dog(String name,String breed)
+	{
+		this.name=name;
+		this.breed=breed;
+	}
+	public String getBreed()
+	{
+		return breed;
+	}
 
 }
